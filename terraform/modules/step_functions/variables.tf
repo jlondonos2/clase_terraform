571f88021bd925931b7a0e9f37c528a17165d947
jlondonos2 <jose.londonos@comunidad.iush.edu.co>
2026-05-14 20:13:31 -0500

    despliegue 2

--- terraform/modules/step_functions/variables.tf
+++ terraform/modules/step_functions/variables.tf
@@ -1,4 +1,14 @@
 
+variable "project" {
+  type        = string
+  description = "Nombre del proyecto"
+}
+
+variable "env" {
+  type        = string
+  description = "Entorno (dev, prod, etc.)"
+}
+
 variable "sfn_role_arn" {
   type        = string
   description = "ARN del rol IAM para Step Functions"
